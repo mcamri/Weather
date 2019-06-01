9ea14d04641aeca1dc765a192b7195c35e28585e
Amri <chozinul@gmail.com>
2019-06-01 20:25:02 +0800

    Initial Commit

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by Muchamad Chozinul Amri on 1/6/19.
+//  Copyright © 2019 Muchamad Chozinul Amri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
